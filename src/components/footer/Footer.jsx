78524afe8f2c0939b3358e85ff6048e8aa91f56d
arzerra <ianclark1506@gmail.com>
2025-08-20 00:49:58 +0800

    header, footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,22 @@
-import style from "./Footer.module.css";
-import { FaFacebookF, FaInstagram, FaXTwitter } from "react-icons/fa6";
-import { MdPhone, MdOutlineMailOutline, MdLocationOn } from "react-icons/md";
+import style from './Footer.module.css';
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
-export default function Footer() {
-  return (
-    <footer className={style.footer}>
-
-
-
-
-      <div className={style.footerBottom}>
-        <p>
-          &copy; {new Date().getFullYear()} Artisan Dental Clinic | All Rights
-          Reserved
-        </p>
-      </div>
-    </footer>
-  );
+export default function Footer(){
+    return(
+        <footer className={style.footer}>
+            <p>&copy; {new Date().getFullYear()} Artisan Dental Clinic | All Rights Reserved</p>
+            
+            <div className={style.footerSocial}>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                    <FaFacebook />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                    <FaInstagram />
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                    <FaTwitter />
+                </a>
+            </div>
+        </footer>
+    );
 }
