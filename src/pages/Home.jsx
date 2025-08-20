9d0aabd652225cbd201962d2734f1dae71ad6893
arzerra <ianclark1506@gmail.com>
2025-08-20 16:09:49 +0800

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+import toothImg from '../assets/images/toothTitle.png';
 
 export default function Home(){
     return(
-        <>
+        <div className={styles.container}>
         <div className={styles.home}>
-
+            <div className={styles.left}>
+                <h1>
+                    "Time Heals Everything"
+                    <br />Except cavities, those need a dentist.
+                </h1>
+                <button>Book Now!</button>
+            </div>
+            <div className={styles.right}>
+                <img src={toothImg} alt="Tooth" />
+            </div>
         </div>
-        </>
+        </div>
     )
 }
