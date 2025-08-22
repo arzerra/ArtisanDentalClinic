3771fb84d47b5e78ee1ed5df4c7fe56220600966
arzerra <ianclark1506@gmail.com>
2025-08-22 11:49:15 +0800

    contactUs

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -7,6 +7,7 @@ export default function ContactUs() {
       <div className={styles.contactBackground}>
         <h1>CONTACT US</h1>
       </div>
+      <h1>How can we help?</h1>
       <div className={styles.contactForm}>
         <h1>Contact Us</h1>
       </div>
