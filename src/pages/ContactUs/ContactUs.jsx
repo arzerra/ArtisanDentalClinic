ddd1739a5213e3f594a84ffb73d6a786c6ed9809
arzerra <ianclark1506@gmail.com>
2025-08-22 11:46:04 +0800

    contactUs

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -2,7 +2,14 @@ import styles from './ContactUs.module.css';
 
 export default function ContactUs() {
   return (
-    <div>
+    <div className={styles.contactContainer}>
+      <div className={styles.contactTopMargin}></div>
+      <div className={styles.contactBackground}>
+        <h1>CONTACT US</h1>
+      </div>
+      <div className={styles.contactForm}>
+        <h1>Contact Us</h1>
+      </div>
     </div>
   );
 }
