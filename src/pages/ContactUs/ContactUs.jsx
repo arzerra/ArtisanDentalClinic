aa604423251934401a2f10e5ae643180d1256ab4
arzerra <ianclark1506@gmail.com>
2025-08-24 14:26:00 +0800

    contactUs

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -7,13 +7,40 @@ export default function ContactUs() {
       <div className={styles.contactBackground}>
         <h1>Contact Us</h1>
       </div>
+
       <h1>How can we help?</h1>
-      <div className={styles.contactForm}>
-        <Form />
-      </div>
-      <div>
-        <h1>Clinic Hours</h1>
-        <span></span>
+
+      {/* Flex row wrapper */}
+      <div className={styles.contactContent}>
+        {/* Left: Form */}
+        <div className={styles.contactForm}>
+          <Form />
+        </div>
+
+        {/* Right: Info */}
+        <div className={styles.contactInfo}>
+          <div>
+            <h1>Clinic Hours</h1>
+            <span>• Morning: 10:00AM - 12:00PM</span><br />
+            <span>• Afternoon: 1:00PM - 5:00PM</span>
+          </div>
+
+          <div>
+            <h1>Contact Number</h1>
+            <span>• 0917 717 5300</span><br />
+            <span>• 0961 943 7592</span>
+          </div>
+
+          <div>
+            <h1>Message Us</h1>
+            <button>Messenger</button>
+          </div>
+
+          <div>
+            <h1>Follow us</h1>
+            <button>Facebook</button>
+          </div>
+        </div>
       </div>
     </div>
   );
