cd367f891d6a706d4061ab9ab3c1dd78943c6bf2
arzerra <ianclark1506@gmail.com>
2025-08-31 10:03:32 +0800

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import styles from './Home.module.css';
 export default function Home(){
     return(
         <div className={styles.homeContainer}>
+            <div className={styles.homeBackground}>
+
+            </div>
         </div>
     )
 }
