c71611da5b49259e671d5b0d3c0a52834b4519a2
arzerra <ianclark1506@gmail.com>
2025-09-03 13:09:35 +0800

    images, header, home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
-import Carousel from "./segments/Carousel.jsx";
+import Carousel from "./components/Carousel.jsx";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-start bg-gray-100 pb-[100px]">
-      <div className="max-w-[1100px] w-full h-auto border border-red-500">
+        {/* Background */}
+<div className="w-full max-h-[200px] overflow-hidden">
+  <img
+    src="/images/pages/home/bg/1.jpg"
+    alt="background"
+    className="w-full h-[200px] object-cover"
+  />
+</div>
+
+
+      <div className="max-w-[1100px] w-full h-auto">
         {/* TITLE */}
-        <div className="h-[350px] mt-40">
-          <h1 className="text-4xl">
+        <div className="container h-[350px] mt-40">
+          <h1 className="text-4xl text-center">
             "Time Heals Everything" Except cavities, those need a dentist,
           </h1>
         </div>
