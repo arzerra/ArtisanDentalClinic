68ba6cfd31f51a7dd6d149c2aacfdde4e33f18f9
arzerra <ianclark1506@gmail.com>
2025-09-05 10:30:31 +0800

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -250,7 +250,7 @@ export default function Home() {
       </div>
 
 
-    <div className="w-[95%] sm:w-full max-w-[1080px] h-auto sm:h-[130px] bg-white shadow-md rounded-2xl mt-20 mx-auto flex flex-col sm:flex-row items-center sm:justify-between justify-center text-center sm:text-center px-6 sm:px-10 py-6 sm:py-6" data-aos="fade-left">
+    <div className="w-[95%] sm:w-full max-w-[1080px] h-auto sm:h-[130px] bg-white shadow-md rounded-2xl mt-20 mx-auto flex flex-col sm:flex-row items-center sm:justify-between justify-center text-center sm:text-center px-6 sm:px-10 py-6 sm:py-6" data-aos="zoom-out">
       <p className="text-gray-700 text-2xl sm:text-4xl font-medium mb-4 sm:mb-0">
         Follow us on Facebook
       </p>
