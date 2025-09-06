996302be691bc02b81216fb002d877a3d7648d1a
arzerra <ianclark1506@gmail.com>
2025-09-07 01:06:40 +0800

    appointment

--- src/pages/appointments/Appointments.jsx
+++ src/pages/appointments/Appointments.jsx
@@ -4,22 +4,23 @@ export default function Appointments(){
     return(
         <>
         <div className="w-full flex flex-col items-center justify-start bg-gray-100 pb-[100px]">
-            <div className="relative w-full max-h-[800px] overflow-hidden">
+            <div className="relative w-full max-h-[900px] overflow-hidden">
                 <img
                     src="/images/pages/appointment/bg/1.jpg"
                     loading="eager"
                     fetchPriority="high"
                     alt="background"
                     className="
-                    object-cover object-[50%_53%] w-full blur-[1.5px] lg:blur-[0.5px]
-                    h-[350px] sm:h-[600px] md:h-[600px] lg:h-[800px]
+                    object-cover object-[50%_53%] w-full blur-[0.5px] lg:blur-[0.5px]
+                    h-[350px] sm:h-[600px] md:h-[600px] lg:h-[900px]
                     "
                 />
             <div className="absolute inset-0 bg-gradient-to-t from-transparent via-black/10 to-black/40"></div>
+            
             </div>
 
 
-            <div className="max-w-[1100px] w-full -mt-25 sm:-mt-50 px-9 sm:px-4 relative z-10">
+            {/* <div className="max-w-[1100px] w-full -mt-25 sm:-mt-25 px-9 sm:px-4 relative z-10">
             <div className="grid grid-cols-1">
                 <div className="bg-white shadow-lg rounded-2xl p-10 sm:p-10 text-center">
                 <h2 className="font-['Archivo_Black'] text-black text-4xl sm:text-4xl font-bold drop-shadow-lg">Our Services</h2>
@@ -34,7 +35,7 @@ export default function Appointments(){
                 <p className="text-gray-600">Providing trusted dental care backed by years of skill and expertise.</p>
                 </div>
             </div>
-            </div>
+            </div> */}
 
         </div>
         </>
