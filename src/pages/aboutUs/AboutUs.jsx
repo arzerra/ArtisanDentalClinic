82f2c5987021e18b9acbce621b2cde05ec9d7eee
arzerra <ianclark1506@gmail.com>
2025-09-08 11:03:18 +0800

    aboutUs

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -34,52 +34,52 @@ export default function AboutUs() {
         </div>
 
         <div className="max-w-[1100px] w-[85%] sm:w-full mt-5">
-          <h1 className="font-['Jura'] mt-5 text-gray-800 drop-shadow-sm text-3xl sm:text-5xl text-center" >
+          <h1 className="font-['Jura'] mt-5 text-gray-800 drop-shadow-sm text-3xl sm:text-5xl text-center" data-aos="fade-up" >
             Welcome to Artisan Dental Clinic
           </h1>
         </div>
         <div className='max-w-[1100px] w-[85%] sm:w-full grid grid-cols-1 sm:grid-cols-2 mt-5 sm:mt-10'>
           <div className="mx-5 mt-1 sm:mt-5">
             <div className="flex items-center justify-center mt-0">
-              <img src="images/logo/adc1.png" alt="Artisan" className="w-120" />
+              <img src="images/logo/adc1.png" alt="Artisan" className="w-120" data-aos="fade-right"/>
             </div>
           </div>
           <div className="mt-2 sm:mt-12 mr-0 sm:mr-15">
-            <h1 className="font-['Darker_Grotesque'] mt-5 text-gray-700 text-2xl sm:text-3xl text-justify sm:text-justify" >
+            <h1 className="font-['Darker_Grotesque'] mt-5 text-gray-700 text-2xl sm:text-3xl text-justify sm:text-justify" data-aos="fade-left">
               Where healthy smiles are crafted with care, comfort, and expertise. We’re here to make your dental journey 
               stress-free and give you every reason to smile with confidence.</h1>
           </div>
         </div>
 
-        <div className="w-full">
+        <div className="w-full" data-aos="fade-up">
           <NumberCounter />
         </div>
 
-        <div className='mt-[10px]'>
+        <div className='mt-[10px]' data-aos="zoom-in">
           <Button />
         </div>
 
         <div className="w-full bg-[url('/images/pages/about/2.jpg')] bg-fixed bg-cover bg-center mt-20 py-20">
           <section className="max-w-[1100px] w-full px-6 sm:px-0 mx-auto ">
-            <h3 className="font-['Jura'] text-gray-800 drop-shadow-sm text-3xl sm:text-4xl text-center mb-6">
+            <h3 className="font-['Jura'] text-gray-800 drop-shadow-sm text-3xl sm:text-4xl text-center mb-6" data-aos="fade-down">
               Our Core Values
             </h3>
             <hr className="w-20 border-t-2 border-black mx-auto mt-2" data-aos="zoom-in-up" />
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center font-['Barlow_Condensed'] text-base sm:text-xl mt-7">
-              <div className="p-4 border border-gray-500 rounded-lg shadow-sm">
-                <h4 className="font-semibold text-gray-700">Compassion & Comfort</h4>
+              <div className="p-4 border border-gray-500 rounded-lg shadow-sm" data-aos="fade-right">
+                <h4 className="font-semibold text-gray-700" >Compassion & Comfort</h4>
                 <p className="mt-2 text-gray-600">
                   We put your comfort first at every step of your dental journey.
                 </p>
               </div>
-              <div className="p-4 border border-gray-500 rounded-lg shadow-sm">
-                <h4 className="font-semibold text-gray-700">Expert Care</h4>
+              <div className="p-4 border border-gray-500 rounded-lg shadow-sm" data-aos="fade-up">
+                <h4 className="font-semibold text-gray-700" >Expert Care</h4>
                 <p className="mt-2 text-gray-600">
                   Our team applies the latest dental techniques with a gentle touch.
                 </p>
               </div>
-              <div className="p-4 border border-gray-500 rounded-lg shadow-sm">
-                <h4 className="font-semibold text-gray-700">Trust & Integrity</h4>
+              <div className="p-4 border border-gray-500 rounded-lg shadow-sm" data-aos="fade-left">
+                <h4 className="font-semibold text-gray-700" >Trust & Integrity</h4>
                 <p className="mt-2 text-gray-600">
                   Delivering honest, personalized care that earns your smile and
                   trust.
@@ -90,7 +90,7 @@ export default function AboutUs() {
         </div>
 
           <div className="max-w-[1080px] w-[90%] sm:w-full h-auto sm:h-[130px] bg-white shadow-md rounded-4xl mt-10 sm:mt-20 mx-auto flex flex-col sm:flex-row items-center sm:justify-between justify-center text-center px-6 sm:px-10 py-6 sm:py-6">
-            <p className="text-gray-700 text-2xl sm:text-3xl font-medium mb-4 sm:mb-0" data-aos="fade">
+            <p className="text-gray-700 text-2xl sm:text-3xl font-medium mb-4 sm:mb-0" data-aos="fade-right">
             Visit us on Instagrams
             </p>
             <a
@@ -110,10 +110,10 @@ export default function AboutUs() {
         </div>
 
         <div className="max-w-[1100px] w-full mt-10 sm:mt-20">
-          <h1 className="font-['Jura'] mt-5 text-gray-800 drop-shadow-sm text-2xl sm:text-4xl text-center" >
+          <h1 className="font-['Jura'] mt-5 text-gray-800 drop-shadow-sm text-2xl sm:text-4xl text-center" data-aos="fade-up">
             See How We Made Them Smile
           </h1>
-          <div className='flex justify-center gap-2 mt-5'>
+          <div className='flex justify-center gap-2 mt-5' data-aos="fade-up">
           <img src="images/pages/about/star.png" alt="star" className='w-10 sm:w-13'/>
           <img src="images/pages/about/star.png" alt="star" className='w-10 sm:w-13'/>
           <img src="images/pages/about/star.png" alt="star" className='w-10 sm:w-13'/>
@@ -123,6 +123,9 @@ export default function AboutUs() {
           <div>
           <ReviewsCarousel/>
           </div>
+          {/* <div>
+          <p>Facebook review: 4 out of 4 reviews.</p>
+          </div> */}
         </div>
 
         <div className="max-w-[1100px] w-full mt-10 sm:mt-20">
