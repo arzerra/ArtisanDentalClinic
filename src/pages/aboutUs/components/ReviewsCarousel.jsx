d174aad242ed6c9029f74fe996ccf8059944dcd2
arzerra <ianclark1506@gmail.com>
2025-09-08 10:27:54 +0800

    ABOUT

--- src/pages/aboutUs/components/ReviewsCarousel.jsx
+++ src/pages/aboutUs/components/ReviewsCarousel.jsx
@@ -48,21 +48,7 @@ function ReviewsCarousel() {
     prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
         breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 640,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
