adee77d603867083b98bd684cb3f5b92094240b4
arzerra <ianclark1506@gmail.com>
2025-10-09 21:49:16 +0800

    implementing sidebar for admin dashboard

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -5,11 +5,13 @@ import LogoutButton from "../../components/button/LogoutButton.jsx";
 import AddPatient from "./components/AddPatient.jsx";
 import CreateUser from "./components/CreateUser.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import { BsLayoutSidebar } from "react-icons/bs";
 
 export default function Admin() {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   useEffect(() => {
     const getSession = async () => {
@@ -43,9 +45,41 @@ export default function Admin() {
 
   return (
     <>
-      <main className="Admin">
-        <Sidebar>{/* <SidebarItem/> */}</Sidebar>
-      </main>
+      <div className="flex bg-gray-100 h-screen">
+        {/* sidebar */}
+        <div
+          className={`fixed bg-white w-64 h-screen shadow ${
+            sidebarOpen ? "-translate-x-0" : "-translate-x-64"
+          } lg:translate-x-0 lg:static`}
+        >
+          <div className="p-4 flex justify-between">
+            <div className="text-xl font-semibold">Logo</div>
+            <div className="lg:hidden" onClick={() => setSidebarOpen(false)}>
+              X
+            </div>
+          </div>
+        </div>
+
+        {/* main content */}
+        <main className="flex-1">
+          <header className="bg-white flex justify-between items-center p-4">
+            <button
+              className="p-2 text-xl lg:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <BsLayoutSidebar />
+            </button>
+            <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
+            <div>
+              <img
+                src="images/icon/sidebar/dentist.png"
+                alt="user icon"
+                className="w-10 rounded-full"
+              />
+            </div>
+          </header>
+        </main>
+      </div>
     </>
   );
 }
--- src/pages/admin/components/Sidebar.jsx
+++ src/pages/admin/components/Sidebar.jsx
@@ -1,35 +1,7 @@
-import { BsLayoutSidebar } from "react-icons/bs";
-import React, { useState } from "react";
+import React from "react";
 
 function Sidebar() {
-  const [collapsed, setCollapsed] = useState(false);
-  return (
-    <>
-      <aside className="h-screen">
-        <nav
-          className={`h-full flex flex-col bg-white border-r rounded-r-2xl shadow-sm transition-all duration-300 ${
-            collapsed ? "w-20" : "w-64"
-          }`}
-        >
-          <div className="p-4 pb-2 flex justify-between items-center">
-            <h1
-              className={`transition-all ${
-                collapsed ? "text-[0px]" : "text-lg"
-              }`}
-            >
-              Admin Dashboard
-            </h1>
-            <button
-              onClick={() => setCollapsed(!collapsed)}
-              className="p-1.5 rounded-lg bg-gray"
-            >
-              <BsLayoutSidebar className="transition-transform text-black" />
-            </button>
-          </div>
-        </nav>
-      </aside>
-    </>
-  );
+  return <div>Sidebar</div>;
 }
 
 export default Sidebar;
