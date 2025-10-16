1ae81efcb9f39506513ebb954aebf4caec6d9d20
arzerra <ianclark1506@gmail.com>
2025-10-16 18:50:14 +0800

    ADMIN

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -98,6 +98,7 @@ export default function Admin() {
           {/* Bottom Section */}
           <div className="p-4 border-t">
             <LogoutButton />
+            <LogoutButton />
           </div>
         </div>
 
